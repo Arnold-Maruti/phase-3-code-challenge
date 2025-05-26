cf1478ca2f4d5a0a772e5c97fa4c17cd96887dfc
Arnold <Arnold>
2025-05-26 22:25:54 +0300

    added mock data to my_database

---
+++ lib/db/seed.py
@@ -0,0 +1,13 @@
+from lib.db.connection import CONN,CURSOR
+
+def seed():
+    CURSOR.execute("INSERT INTO authors (name) VALUES ('Alice')")
+    CURSOR.execute("INSERT INTO authors (name) VALUES ('Bob')")
+
+    CURSOR.execute("INSERT INTO magazines (name, category) VALUES ('Tech Monthly', 'Technology')")
+    CURSOR.execute("INSERT INTO magazines (name, category) VALUES ('Nature Weekly', 'Science')")
+
+    CURSOR.execute("INSERT INTO articles (title, author_id, magazine_id) VALUES ('AI Revolution', 1, 1)")
+    CURSOR.execute("INSERT INTO articles (title, author_id, magazine_id) VALUES ('Climate Change', 2, 2)")
+
+    CONN.commit()
