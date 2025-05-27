729e7abfd6a9e411475754ca34d2e9cfecaa9d9c
Arnold <Arnold>
2025-05-27 10:14:07 +0300

    final change

--- lib/debug.py
+++ lib/debug.py
@@ -1,13 +1,12 @@
 from lib.models import article,author,magazine
 
 # print(article.Article.find_by_id(2))
-hr=author.Author(4,"jersey")
-hr.save()
+# hr=author.Author(4,"jersey")
+# hr.save()
 
-biggie=article.Article(2,"biggie",4,3)
-biggie.save()
+# biggie=article.Article(2,"biggie",4,3)
+# biggie.save()
 
-timesnewyork=magazine.Magazine(3,"timesnewyork","action")
-timesnewyork.save()
+# timesnewyork=magazine.Magazine(3,"timesnewyork","action")
+# timesnewyork.save()
 
-print(hr.articles())
